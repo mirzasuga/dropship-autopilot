fc2210b9172301bce7e9246456a600c91ffc7d8a
mirza <sugamirza2@gmail.com>
2024-12-06 08:04:41 +0700

    Plasmo Setup

---
+++ src/popup.tsx
@@ -0,0 +1,24 @@
+import { useState } from "react"
+
+function IndexPopup() {
+  const [data, setData] = useState("")
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        padding: 16,
+        width: 400,
+        height: 400,
+      }}>
+      <h1>
+        Welcome to your <a href="https://www.plasmo.com">Plasmo</a> Extension!
+      </h1>
+      <input onChange={(e) => setData(e.target.value)} value={data} />
+      <footer>Crafted by @PlasmoHQ</footer>
+    </div>
+  )
+}
+
+export default IndexPopup
